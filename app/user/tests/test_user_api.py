ed93f37de0bb25087f5899a1c81875b7891f5a99
japheth <japhemchirie@gmail.com>
2023-12-11 18:16:22 +0300

    manage user account and create recipe tests

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -123,7 +123,7 @@ class PrivateUserApiTests(TestCase):
             name="Test Name",
         )
         self.client = APIClient()
-        self.client.force_authentication(user=self.user)
+        self.client.force_authenticate(user=self.user)
 
     def test_retrieve_profile_success(self):
         """Test retrieving profile for the logged in user."""
