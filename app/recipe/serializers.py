400d954ac38bea99f3a858bea1162d1c659feb1e
japheth <japhemchirie@gmail.com>
2023-12-12 09:09:53 +0300

    recipe serializers

---
+++ app/recipe/serializers.py
@@ -0,0 +1,15 @@
+"""
+Serializers for Recipe APIs.
+"""
+from rest_framework import serializers
+
+from core.models import Recipe
+
+
+class RecipeSerializer(serializers.ModelSerializer):
+    """Serializers for recipes."""
+
+    class Meta:
+        model = Recipe
+        fields = ['id', 'title', 'time_minutes', 'price', 'links']
+        read_only_fields = ['id']
