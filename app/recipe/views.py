5eeb9b49e07c3361b977259890fa69ea6a6f5265
japheth <japhemchirie@gmail.com>
2023-12-14 11:31:43 +0300

    extend openapi schema documentation for filtering recipes

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -21,6 +21,22 @@ from core.models import Ingredient, Recipe, Tag
 from recipe import serializers
 
 
+@extend_schema_view(
+    list=extend_schema(
+        parameters=[
+            OpenApiParameter(
+                'tags',
+                OpenApiTypes.STR,
+                description='Comma separated list of tag IDs to filter',
+            ),
+            OpenApiParameter(
+                'ingredients',
+                OpenApiTypes.STR,
+                description='Comma separated list of ingredient IDs to filter',
+            ),
+        ]
+    )
+)
 class RecipeViewSet(viewsets.ModelViewSet):
     """View for manage recipe APIs."""
     serializer_class = serializers.RecipeDetailSerializer
